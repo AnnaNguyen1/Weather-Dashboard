7d39c82500494d3ee8da46ae4ada3a1b23ea9ffb
Anna <an9062@gmail.com>
2021-09-25 12:20:21 +1000

    adding error messages to an empty search field

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,18 +1,27 @@
-var searchForm = document.querySelector("#search-form");
+var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
+var errorMessage = document.querySelector('#error');
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
 
-    var searchInputVal= searchInput.val.trim();
+    var searchInputVal= searchInput.value.trim();
 
-    if (!searchInputVal) {
-        var error = document.createElement("p");
-        error.textContent = "Please enter a city";
-        searchInput.appendChild(error);
-    };
-
-    var queryString = './search-results.html?q=' + searchInputVal;
+    if (searchInputVal) {
+        getWeatherData(searchInputVal)
+        console.log("work");
+        searchInputVal.textContent = '';
+    } else {
+        searchInput.setAttribute('class', 'error-box');
+        errorMessage.style.display = 'block';
+    }
 };
 
+function removeError() {
+        searchInput.removeAttribute('class');
+        errorMessage.style.display = 'none';
+}
+    
+
 searchForm.addEventListener('submit', handleSearchFormSubmit);
+searchInput.addEventListener('keyup', removeError())
