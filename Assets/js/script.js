1a35e70cfb65f69d844adf94301732e0363769dd
Anna <an9062@gmail.com>
2021-09-25 14:53:59 +1000

    retrieve API data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
 var errorMessage = document.querySelector('#error');
+var currentTemp = document.querySelector('#current-temp-container');
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
@@ -11,17 +12,54 @@ function handleSearchFormSubmit(event) {
         getWeatherData(searchInputVal)
         console.log("work");
         searchInputVal.textContent = '';
+        searchInput.removeAttribute('class');
+        errorMessage.style.display = 'none';
     } else {
         searchInput.setAttribute('class', 'error-box');
         errorMessage.style.display = 'block';
     }
 };
 
-function removeError() {
-        searchInput.removeAttribute('class');
-        errorMessage.style.display = 'none';
+function getWeatherData(city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city.toLowerCase() + '&appid=9382f6da9ce61a40176360cba8a34a63&units=metric'
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(function (response) {
+            console.log(response);
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayWeather(data);
+                });
+            } else {
+                currentTemp.textContent = '';
+
+                var errorCityName = document.createElement('p');
+                errorCityName.textContent = "Weather data cannot be found for " + city;
+                currentTemp.appendChild(errorCityName);
+            }
+        })
+        .catch(function (error) {
+            var errorCityName = document.createElement('p');
+            errorCityName.textContent = "Unable to connect to Open Weather App";
+            currentTemp.appendChild(errorCityName);
+        })
 }
-    
+
+function displayWeather(weather) {
+    console.log(weather);
+    currentTemp.textContent = '';
+    var day = moment().format('DD-MM-YYYY');
+
+    var cityName = document.createElement('p');
+    cityName.textContent = "Current Temp for " + weather.name + " " + "(" + day + ")";
+    currentTemp.appendChild(cityName);
+
+    var currTemp = document.createElement('p');
+    currTemp.textContent = weather.main.temp;
+    console.log(currTemp);
+    currentTemp.appendChild(currTemp);
+
+};
 
 searchForm.addEventListener('submit', handleSearchFormSubmit);
-searchInput.addEventListener('keyup', removeError())
